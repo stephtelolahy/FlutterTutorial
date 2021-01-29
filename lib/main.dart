fcf95594e98361321e0a510d7326730ffa31768e
Stéphano TELOLAHY <hugues.stephano.telolahy@devoteam.com>
2021-01-29 22:55:07 +0100

    save old lessons

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'layoutdemo.dart';
+import 'package:flutter_app/firstapp.dart';
+import 'package:flutter_app/layouts.dart';
+import 'package:flutter_app/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Flutter layout demo',
     home: LayoutDemo(),
+    // home: WidgetsDemo(),
+    // home: RandomWords(),
   ));
 }
