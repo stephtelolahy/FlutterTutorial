0a67ea8cd81ed0eb6e2b8c45ab5e51905a842491
Stéphano TELOLAHY <hugues.stephano.telolahy@devoteam.com>
2021-01-29 23:28:41 +0100

    interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/firstapp.dart';
+import 'package:flutter_app/interactivity.dart';
 import 'package:flutter_app/layouts.dart';
+import 'package:flutter_app/tapboxb.dart';
 import 'package:flutter_app/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Flutter layout demo',
-    home: LayoutDemo(),
+    // home: LayoutDemo(),
     // home: WidgetsDemo(),
     // home: RandomWords(),
+    // home: InteractivityDemo(),
+    home: ParentWidget()
   ));
 }
---
+++ lib/tapboxb.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+// The parent widget manages the widget’s state
+// https://flutter.dev/docs/development/ui/interactive#the-parent-widget-manages-the-widgets-state
+
+//------------------------ ParentWidget --------------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//------------------------- TapboxB ----------------------------------
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[800] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
